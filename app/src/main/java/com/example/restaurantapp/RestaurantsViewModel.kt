8efde5ad4fb4d5f05843d21fd5cd2620e05c2b38
soongonkim <soongon@gmail.com>
2023-08-21 21:58:44 +0900

    retrofit api call from firebase

--- app/src/main/java/com/example/restaurantapp/RestaurantsViewModel.kt
+++ app/src/main/java/com/example/restaurantapp/RestaurantsViewModel.kt
@@ -3,6 +3,9 @@ package com.example.restaurantapp
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -21,9 +24,25 @@ class RestaurantsViewModel(): ViewModel() {
     }
 
     fun getRestaurants() {
-        restInterface.getRestaurants().execute().body()?.let { restaurants ->
-            state.value = restaurants
-        }
+        restInterface.getRestaurants().enqueue(
+            object : Callback<List<Restaurant>> {
+                override fun onResponse(
+                    call: Call<List<Restaurant>>,
+                    response: Response<List<Restaurant>>
+                ) {
+                    response.body()?.let { restaurants ->
+                        state.value = restaurants
+                    }
+                }
+                override fun onFailure(
+                    call: Call<List<Restaurant>>,
+                    t: Throwable
+                ) {
+                    t.printStackTrace()
+                }
+
+            }
+        )
     }
 
     // 레스토랑 id 값으로 해당 레스토랑의 isFavorite 값을 변경
